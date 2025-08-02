81cb7835a92eba0ad935466212e9ce3f5d8d2cca
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-02 21:35:05 +0800

    03_whatIsJSX: Creating an Accordion Component using Material UI.

---
+++ 03_whatIsJSX/08_accordion/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Accordion from "./components/AccordionComponent";
+
+const App = () => {
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+};
+
+export default App;
