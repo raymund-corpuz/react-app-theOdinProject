919c53ca8e974474c19d5017c0e1c75e31d7a6f3
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-03 09:26:18 +0800

    03_whatIsJSX: Creating a Multi Step Component.

---
+++ 03_whatIsJSX/10_multiStep/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MultiStep from "./components/MultiStep";
+
+const App = () => {
+  return (
+    <div>
+      <MultiStep />
+    </div>
+  );
+};
+
+export default App;
