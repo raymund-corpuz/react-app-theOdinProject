64285ff54b1d163a97e31c357f79d84122664449
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-07 14:17:38 +0800

    09_cvProject: Creating a CV Builder using React. When Submitting the value from the input field does not reset.

--- 09_cvProject/01_firstCVProject/src/components/CvInput.jsx
+++ 09_cvProject/01_firstCVProject/src/components/CvInput.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-const CvInput = ({ data }) => {
+const CvInput = ({ data, onChange }) => {
   return (
     <div className="mb-1  ">
       <label htmlFor="" className={data.label}>
-        {data.name}:
+        {data.title}:
       </label>
-      <input type={data.type} className={data.input} />
+      <input
+        name={data.name}
+        type={data.type}
+        className={data.input}
+        onChange={onChange}
+        value={data.value}
+      />
     </div>
   );
 };
--- 09_cvProject/01_firstCVProject/src/components/NewCvForm.jsx
+++ 09_cvProject/01_firstCVProject/src/components/NewCvForm.jsx
@@ -1,19 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import data from "../data/data.js";
 import CvInput from "./CvInput.jsx";
 
 const NewCvForm = () => {
+  const [user, setUser] = useState({
+    fullName: "",
+    profession: "",
+    email: "",
+    phone: "",
+    address: "",
+    linkedIn: "",
+    gitHub: "",
+    personalSummary: "",
+    jobTitle: "",
+    company: "",
+    companyLocation: "",
+    companyStart: "",
+    companyEnd: "",
+    jobDescription: "",
+    degree: "",
+    school: "",
+    schoolLocation: "",
+    schoolStart: "",
+    schoolEnd: "",
+    additionalInfo: "",
+    projectTitle: "",
+    projectDescription: "",
+    technologiesUsed: "",
+    projectURL: "",
+    role: "",
+  });
+  const { name, value } = user;
+
+  const [userCollection, setUserCollection] = useState([]);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newUser = { ...user, id: Date.now() };
+    setUserCollection([...userCollection, newUser]);
+    setUser({ ...user, id: "" });
+  }
+
+  useEffect(() => {
+    console.log(user);
+    console.log(userCollection);
+  }, [user, userCollection]);
   return (
     <div className="container">
       <div className="text-center">
         <h1>CV Application</h1>
       </div>
-      <form action="" className="px-4 pb-4">
+      <form action="" className="px-4 pb-4" onSubmit={handleSubmit}>
         {data.map((item) => (
           <>
             <h3>{item.section}</h3>
             {item.fields.map((each) => (
-              <CvInput data={each} />
+              <CvInput data={each} onChange={handleChange} key={value} />
             ))}
           </>
         ))}
