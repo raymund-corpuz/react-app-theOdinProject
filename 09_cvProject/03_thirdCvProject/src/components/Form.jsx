46c35dd59fc5d2c2926740f7197fffbfa9653402
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-10 08:34:03 +0800

    09_cvProject: Creating Submit and Next Button also functionality is done. making the Layout is already okay.

--- 09_cvProject/03_thirdCvProject/src/components/Form.jsx
+++ 09_cvProject/03_thirdCvProject/src/components/Form.jsx
@@ -11,26 +11,56 @@ export default function Form() {
   const [step, setStep] = useState(0);
   const [user, setUser] = useState(initialState);
 
+  function handleNext(e) {
+    e.preventDefault();
+    setStep((prevState) => prevState + 1);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (step > data.length - 1) {
+      console.log("next");
+    } else {
+      alert("Form Submitted");
+    }
+  }
+
   return (
-    <div>
+    <div className="container-xxl">
       <h1>Form</h1>
-      <FormSection section={data[step]} user={user} />
+      <FormSection
+        section={data[step]}
+        user={user}
+        step={step}
+        onNext={handleNext}
+        onSubmit={handleSubmit}
+      />
     </div>
   );
 }
 
-function FormSection({ section, user }) {
+function FormSection({ section, user, step, onNext, onSubmit }) {
   return (
-    <div>
+    <form className="col-5">
       <h3>{section.section}</h3>
 
       {section.fields.map((field) => (
-        <div key={field.name}>
-          <label htmlFor="">{field.title}</label>
-          <input type={field.type} value={user[field.name] || ""} />
+        <div key={field.name} className="my-2">
+          <label>{field.title}</label>
+          <input
+            className={field.input}
+            type={field.type}
+            value={user[field.name] || ""}
+          />
         </div>
       ))}
-    </div>
+
+      {step >= data.length - 1 ? (
+        <Submit onSubmit={onSubmit} />
+      ) : (
+        <Next onNext={onNext} />
+      )}
+    </form>
   );
 }
 
@@ -40,5 +70,21 @@ function FormInput() {
       <label htmlFor=""></label>
       <input type="text" />
     </div>
+  );
+}
+
+function Next({ onNext }) {
+  return (
+    <button className="btn btn-primary" onClick={onNext}>
+      Next
+    </button>
+  );
+}
+
+function Submit({ onSubmit }) {
+  return (
+    <button className="btn btn-primary" onClick={onSubmit}>
+      Submit
+    </button>
   );
 }
