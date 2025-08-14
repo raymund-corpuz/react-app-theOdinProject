5979810b773bd5fe2acd6b0245a0683a68489d96
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-14 20:40:42 +0800

    12_classComponents : Adding Edit and updateItem button.

--- 12_classComponents/01_todoClass/src/components/ClassInput.jsx
+++ 12_classComponents/01_todoClass/src/components/ClassInput.jsx
@@ -6,57 +6,114 @@ class ClassInput extends Component {
 
     this.state = {
       todos: [],
-      inputVal: "",
-      id: Date.now(),
+      newTaskText: "",
+      editing: false,
+      currentid: "",
+      currentValue: "",
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleDelete = this.handleDelete.bind(this);
+    // this.handleEdit = this.handleEdit.bind(this);
+    // this.handleEditInputChanger = this.handleEditInputChanger(this);
+    // this.handleEditTodo = this.handleEditTodo(this);
+    // this.handleSubmitEdit = this.handleSubmitEdit(this);
   }
 
   handleInputChange(e) {
-    this.setState((state) => ({
-      ...state,
-      inputVal: e.target.value,
-    }));
+    // onChange
+    this.setState({ newTaskText: e.target.value });
   }
 
   handleSubmit(e) {
+    // onAddTask
     e.preventDefault();
-    this.setState((state) => ({
-      todos: state.todos.concat(state.inputVal),
-      inputVal: "",
-    }));
-    console.log(this.todos);
+
+    const obj = {
+      name: this.state.newTaskText,
+      id: Date.now(),
+    };
+
+    if (this.state.newTaskText !== "") {
+      this.setState({ todos: this.state.todos.concat(obj) });
+      this.setState({ newTaskText: "" });
+    }
+  }
+
+  handleDelete(itemId) {
+    // onDeleteTask
+    this.setState({
+      todos: [...this.state.todos].filter((id) => id.id !== itemId),
+    });
+  }
+  handleEditTodo(id, newValue) {
+    // onEditTodo
+    this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.name = newValue;
+      }
+    });
+  }
+  handleSubmitEdit(e) {
+    // onSubmitEditTodo
+    e.preventDefault();
+    this.handleEditTodo(this.state.currentid, this.state.currentValue);
+    this.setState({ editing: false });
+  }
+
+  handleEdit(todo) {
+    // onToggleEdit
+    this.setState({ editing: true });
+    this.setState({ currentid: todo.id });
+    this.setState({ currentValue: todo.name });
+  }
+
+  handleEditInputChanger(e) {
+    this.setState({ currentValue: e.target.value });
   }
 
   render() {
     return (
       <section>
-        <h3>{this.props.name}</h3>
-
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="task-entry">Enter a task:</label>
-          <input
-            type="text"
-            id="task-entry"
-            name="task-entry"
-            value={this.state.inputVal}
-            onChange={this.handleInputChange}
-          />
-          <button type="submit">Submit</button>
-        </form>
-
-        <h4>All the tasks!</h4>
-
-        <ul>
-          {this.state.todos.map((todo) => (
-            <li key={todo}>
-              {todo}
-              <button>delete</button>
-            </li>
-          ))}
-        </ul>
+        {this.state.editing === false ? (
+          <>
+            <h3>Todo List</h3>{" "}
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor="task-entry">Enter a task:</label>
+              <input
+                type="text"
+                id="task-entry"
+                name="task-entry"
+                value={this.state.newTaskText}
+                onChange={this.handleInputChange}
+              />
+              <button type="submit">Submit</button>
+            </form>
+            <h4>All the tasks!</h4>
+            <ul>
+              {this.state.todos.map((todo) => (
+                <li key={todo}>
+                  {todo.name}
+                  <button onClick={() => this.handleEdit(todo.id)}>edit</button>
+                  <button onClick={() => this.handleDelete(todo.id)}>
+                    delete
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <form onSubmit={this.handleSubmitEdit}>
+            <input
+              type="text"
+              value={this.state.currentValue}
+              name={this.state.currentValue}
+              onChange={this.handleEditInputChanger}
+            />
+            <button onClick={this.handleSubmitEdit}>Update Item</button>
+          </form>
+        )}
       </section>
     );
   }
