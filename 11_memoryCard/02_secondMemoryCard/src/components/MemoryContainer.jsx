a8cc9932e47c292eee4e9101d7bb7079b321d6ac
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-14 08:37:18 +0800

    11_memoryCard : Displaying all the fetch Pokemon on the screen and adding some styles.

--- 11_memoryCard/02_secondMemoryCard/src/components/MemoryContainer.jsx
+++ 11_memoryCard/02_secondMemoryCard/src/components/MemoryContainer.jsx
@@ -1,38 +1,47 @@
 import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
 
 const MemoryContainer = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     let isMounted = true;
     let controller = new AbortController();
+
     async function fetchPokemon() {
       try {
-        if (isMounted) {
-          const response = await fetch("https://pokeapi.co/api/v2/pokemon", {
-            signal: controller.signal,
-          });
-          const data = await response.json();
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=20",
+          { signal: controller.signal }
+        );
+        const data = await response.json();
 
-          const pokemonData = await Promise.all(data.results.map)(
-            async (pokemonItem) => {
-              const res = await fetch(pokemonItem.url, {
-                signal: controller.signal,
-              });
-              const details = await res.json();
-              return {
-                name: details.name,
-                image: details.sprites.other["official_artwork"].front_default,
-                id: details.id,
-              };
-            }
-          );
+        const pokemonData = await Promise.all(
+          data.results.map(async (pokemon) => {
+            const res = await fetch(pokemon.url, { signal: controller.signal });
+            const details = await res.json();
+            return {
+              name: details.name,
+              image: details.sprites.other["official-artwork"].front_default,
+              id: details.id,
+            };
+          })
+        );
+
+        if (isMounted) {
           setPokemons(pokemonData);
         }
       } catch (error) {
-        console.error("Error occured: ", error.message);
+        if (error.name !== "AbortError") {
+          console.error("Error Occured", error);
+        }
       } finally {
-        isMounted = false;
+        if (isMounted) {
+          setTimeout(() => {
+            setIsLoading(false);
+          }, 1000);
+        }
       }
     }
     fetchPokemon();
@@ -42,20 +51,85 @@ const MemoryContainer = () => {
       controller.abort();
     };
   }, []);
+
+  //   useEffect(() => {
+  //     let isMounted = true;
+  //     let controller = new AbortController();
+  //     async function fetchPokemon() {
+  //       try {
+  //         if (isMounted) {
+  //           const response = await fetch("https://pokeapi.co/api/v2/pokemon", {
+  //             signal: controller.signal,
+  //           });
+  //           const data = await response.json();
+
+  //           const pokemonData = await Promise.all(data.results.map)(
+  //             async (pokemonItem) => {
+  //               const res = await fetch(pokemonItem.url, {
+  //                 signal: controller.signal,
+  //               });
+  //               const details = await res.json();
+  //               return {
+  //                 name: details.name,
+  //                 image: details.sprites.other["official_artwork"].front_default,
+  //                 id: details.id,
+  //               };
+  //             }
+  //           );
+  //           setPokemons(pokemonData);
+  //         }
+  //       } catch (error) {
+  //         console.error("Error occured: ", error.message);
+  //       } finally {
+  //         isMounted = false;
+  //       }
+  //     }
+  //     fetchPokemon();
+
+  //     return () => {
+  //       isMounted = false;
+  //       controller.abort();
+  //     };
+  //   }, []);
   return (
     <div>
       <h1>MemoryContainer</h1>
-      <MemoryCard />
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {!isLoading ? (
+          pokemons.map((pokemon) => (
+            <MemoryCard
+              name={pokemon.name}
+              id={pokemon.id}
+              key={pokemon.id}
+              image={pokemon.image}
+            />
+          ))
+        ) : (
+          <Loader />
+        )}
+      </div>
     </div>
   );
 };
 
 export default MemoryContainer;
 
-function MemoryCard() {
+function MemoryCard({ name, id, image }) {
+  const properCase = name.charAt(0).toUpperCase() + name.slice(1);
   return (
-    <div>
-      <h1>Memory Card</h1>
+    <div
+      style={{
+        border: "1px solid gray",
+        padding: "0.5rem",
+        margin: "1rem",
+        borderRadius: "0.5rem",
+      }}
+    >
+      <h4 style={{ margin: "0", textAlign: "center" }}>
+        {" "}
+        <span>{id}.</span> {properCase}
+      </h4>
+      <img src={image} alt={name} width={"100px"} />
     </div>
   );
 }
