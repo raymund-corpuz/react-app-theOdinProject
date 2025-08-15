2f96a088cb69e5cd6e92b666142f7da27993a14a
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-15 20:11:45 +0800

    13_componentLifecycle: Migrating a simple component from a class to a function.

---
+++ 13_componentLifecycle/02_component/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Greeting from "../src/components/Greeting.jsx";
+const App = () => {
+  return (
+    <div>
+      <Greeting name="Raymund" />
+      <Greeting name="Sara" />
+      <Greeting name="Edith" />
+    </div>
+  );
+};
+
+export default App;
