5cb65136c76701c81dcbaa9db7848115fbfd6b93
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-16 08:47:16 +0800

    14_reactTesting: Implementing Vitest in App.jsx

--- 14_reactTesting/01_firstTest/src/App.jsx
+++ 14_reactTesting/01_firstTest/src/App.jsx
@@ -1,3 +1,7 @@
-export default function App({ title = "React" }) {
-  return <h1>{title}</h1>;
-}
+// export default function App({ title = "React" }) {
+//   return <h1>{title}</h1>;
+// }
+
+const App = () => <h1>Our First Test</h1>;
+
+export default App;
--- 14_reactTesting/01_firstTest/src/test/App.test.jsx
+++ 14_reactTesting/01_firstTest/src/test/App.test.jsx
@@ -9,14 +9,25 @@
 //   });
 // });
 
-import { render, screen } from "@testing-library/react";
+// import { render, screen } from "@testing-library/react";
 
+// import App from "../App";
+
+// describe("App", () => {
+//   it("renders headline", () => {
+//     render(<App title="React" />);
+
+//     expect(screen.getByRole("heading", { name: /react/i })).toBeInTheDocument();
+//   });
+// });
+
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-describe("App", () => {
-  it("renders headline", () => {
-    render(<App title="React" />);
-
-    expect(screen.getByRole("heading", { name: /react/i })).toBeInTheDocument();
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
 });
