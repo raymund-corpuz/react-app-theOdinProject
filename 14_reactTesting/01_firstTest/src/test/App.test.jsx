848fc9f60dfa7f5b977007386f91d08bd086e9ac
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-16 09:28:13 +0800

    14_reactTesting: Testing the react code using event handlers.

--- 14_reactTesting/01_firstTest/src/test/App.test.jsx
+++ 14_reactTesting/01_firstTest/src/test/App.test.jsx
@@ -21,13 +21,48 @@
 //   });
 // });
 
+// import { describe, it, expect } from "vitest";
+// import { render, screen } from "@testing-library/react";
+// import App from "../App";
+
+// describe("App component", () => {
+//   it("renders correct heading", () => {
+//     render(<App />);
+//     expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+//   });
+// });
+
+// import { describe, it, expect } from "vitest";
+// import { render, screen } from "@testing-library/react";
+// import App from "../App";
+
+// describe("App component", () => {
+//   it("render correct heading", () => {
+//     render(<App />);
+
+//     expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+//   });
+// });
+
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../App";
 
-describe("App component", () => {
-  it("renders correct heading", () => {
+describe("App Component", () => {
+  it("renders magnificent monkeys", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
     render(<App />);
-    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
