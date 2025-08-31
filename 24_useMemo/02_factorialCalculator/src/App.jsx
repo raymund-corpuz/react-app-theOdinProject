ac789f2639dbbeb78cc79255d3b6089cdfd34ca5
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-08-31 20:20:05 +0800

    24_useMemo: Creating a Factorial Calculator using the useMemo.

---
+++ 24_useMemo/02_factorialCalculator/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FactorialCalculator } from "./components/FactorialCalculator";
+
+const App = () => {
+  return (
+    <div>
+      <FactorialCalculator />
+    </div>
+  );
+};
+
+export default App;
---
+++ 24_useMemo/02_factorialCalculator/src/components/FactorialCalculator.jsx
@@ -0,0 +1,37 @@
+import { useState, useMemo } from "react";
+
+function factorial(n) {
+  console.log("Calculating factorial ...", n + 1);
+
+  if (n <= 1) return 1;
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    for (let j = 0; j < 100000; j++) {}
+    result *= i;
+  }
+  return result;
+}
+
+export function FactorialCalculator() {
+  const [number, setNumber] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const fact = useMemo(() => factorial(number), [number]);
+  return (
+    <div>
+      <h2>factorial Calculator</h2>
+
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(Number(e.target.value))}
+      />
+
+      <p>
+        Factorial of {number} is: <b>{fact}</b>
+      </p>
+
+      <button onClick={() => setCount(count + 1)}>Re-render ({count})</button>
+    </div>
+  );
+}
